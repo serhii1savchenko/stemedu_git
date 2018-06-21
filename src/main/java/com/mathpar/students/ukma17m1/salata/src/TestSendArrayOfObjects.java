b04ce64ff68c585237fc93a1143182ddd6baaccc
salatakirill <salatakirill@localhost>
2018-06-21 09:43:31 +0300

    changes in chapt 4

--- src/main/java/com/mathpar/students/ukma17m1/salata/src/TestSendArrayOfObjects.java
+++ src/main/java/com/mathpar/students/ukma17m1/salata/src/TestSendArrayOfObjects.java
@@ -3,25 +3,33 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-import mpi.*;
+package com.mathpar.students.ukma17m1.salata.src;
+import mpi.MPI;
+import mpi.MPIException;
+import mpi.Status;
+
 import java.io.*;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import com.mathpar.students.llp2.student.helloworldmpi.Transport;
 /**
  *
  * @author leximiro
  */
 public class TestSendArrayOfObjects {
     
-    //mpirun -np 2 java -cp /home/leximiro/Downloads/stemedu/stemedu/src/main/java/com/mathpar/students/ukma17m1/salata/src/testSendArrayOfObjects param1 param2
-    
+	//mpirun -np 2 java -cp /home/leximiro/Desktop/stemedu/target/classes com/mathpar/students/ukma17m1/salata/src/TestSendArrayOfObjects param1 param2    
    
     public static void main(String[] args) throws MPIException{
-        MPI.init;
+        MPI.Init(args);
         sendObjects(args,1,1);
         Object[] arr = recvObjects(args.length, 1, 1);
         for(Object obj : arr){
             System.out.println(obj);
         }
-        MPI.Finilize();
+        MPI.Finalize();
     }
     
     
