26e74a86015f33f3c7a75a392ed334317c04f178
ness-x540sa <nastia.nastia.zh@gmail.com>
2018-11-21 18:50:59 +0200

    commiting first part of module 2

---
+++ src/main/java/com/mathpar/students/ukma/Zhyrkova/Module2/src/HelloWorldParallel.java
@@ -0,0 +1,21 @@
+import mpi.*;
+
+public class HelloWorldParallel {
+
+    public static void main(String[] args) throws MPIException {
+        // Initialize a parallel part
+        MPI.Init(args);
+        // Define a number of a process
+        int myrank = MPI.COMM_WORLD.getRank();
+        System.out.println("Proc num " + myrank + " Hello World");
+        // Completion a parallel part
+        MPI.Finalize();
+    }
+
+}
+
+// Output for amount of processors equals 4
+//        Proc num 1 Hello World
+//        Proc num 2 Hello World
+//        Proc num 0 Hello World
+//        Proc num 3 Hello World
