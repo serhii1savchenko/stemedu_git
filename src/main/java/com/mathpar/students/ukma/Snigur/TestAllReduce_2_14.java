3414f81036199e3f1bf7e5a7568b618a1d08962d
lavronsashka <lavronsashka@gmail.com>
2018-11-25 21:44:40 +0200

    Snigur, Vakulov, Zadorozhnyi module 2 added

---
+++ src/main/java/com/mathpar/students/ukma/Snigur/TestAllReduce_2_14.java
@@ -0,0 +1,53 @@
+import mpi.MPI;
+import mpi.MPIException;
+
+public class TestAllReduce_2_14 {
+    public static void main(String[] args) throws MPIException {
+        // èíèöèàëèçàöèÿ MPI
+        MPI.Init(args);
+        //îïðåäåëåíèå íîìåðà ïðîöåññîðà
+        int myrank = MPI.COMM_WORLD.getRank();
+        // Definition a processor amount in a group
+        int np = MPI.COMM_WORLD.getSize();
+        int n = Integer.parseInt(args[0]);
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = i;
+        }
+        int[] q = new int[n];
+        MPI.COMM_WORLD.allReduce(a, q, n, MPI.INT, MPI.PROD);
+        for (int j = 0; j < np; j++) {
+            if (myrank == j) {
+                System.out.println("myrank = " + j);
+                for (int i = 0; i < q.length; i++) {
+                    System.out.println(" " + q[i]);
+                }
+            }
+        }
+        //çàâåðøåíèå ïàðàëëåëüíîé ÷àñòè
+        MPI.Finalize();
+    }
+}
+
+// Output:
+//        myrank = 1
+//        myrank = 2
+//        myrank = 3
+//        0
+//        1
+//
+//        0
+//        1
+//        0
+//        1
+//        myrank = 0
+//        0
+//        1
+
+// Output for two processors and n=2
+//        myrank = 0
+//        0
+//        1
+//        myrank = 1
+//        0
+//        1
---
+++ src/main/java/com/mathpar/students/ukma/Vakulov/Module2.2/TestBcast.java
@@ -0,0 +1,36 @@
+import java.util.Arrays;
+import java.util.Random;
+import mpi.*;
+
+public class TestBcast {
+    public static void main(String[] args)
+            throws MPIException {
+        MPI.Init(args);
+        int myrank = MPI.COMM_WORLD.getRank();
+        int n = 4;
+        if (args.length != 0)
+        {
+            n = Integer.parseInt(args[0]);
+        }
+        double[] a = new double[n];
+        if (myrank == 0) {
+            for (int i = 0; i < n; i++) {
+                a[i] = new Random().nextDouble();
+            }
+            System.out.println("myrank = " + myrank + " : a = "
+                    + Arrays.toString(a));
+        }
+        MPI.COMM_WORLD.barrier();
+        MPI.COMM_WORLD.bcast(a, a.length, MPI.DOUBLE, 0);
+        if (myrank != 0)
+            System.out.println("myrank = " + myrank + " : a = "
+                    + Arrays.toString(a));
+        MPI.Finalize();
+    }
+}
+
+// Output for 4 processors and n = 4
+//    myrank = 0 : a = [0.1320077635416097, 0.9476896586791758, 0.32296007767227797, 0.16813548383931642]
+//    myrank = 3 : a = [0.1320077635416097, 0.9476896586791758, 0.32296007767227797, 0.16813548383931642]
+//    myrank = 2 : a = [0.1320077635416097, 0.9476896586791758, 0.32296007767227797, 0.16813548383931642]
+//    myrank = 1 : a = [0.1320077635416097, 0.9476896586791758, 0.32296007767227797, 0.16813548383931642]
