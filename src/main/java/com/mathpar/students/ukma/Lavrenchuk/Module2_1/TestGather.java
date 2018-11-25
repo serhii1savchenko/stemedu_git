e14094b53939fc2275f485d66947b1afef748c78
lavronsashka <lavronsashka@gmail.com>
2018-11-25 21:23:44 +0200

    Lavrenchuk module 2 added

---
+++ src/main/java/com/mathpar/students/ukma/Lavrenchuk/Module2_1/TestGather.java
@@ -0,0 +1,51 @@
+import mpi.*;
+
+public class TestGather {
+    public static void main(String[] args) throws Exception {
+        // Initialization MPI
+        MPI.Init(args);
+        // Definition a processor amount
+        int myrank = MPI.COMM_WORLD.getRank();
+        // Definition a processor amount in a group
+        int np = MPI.COMM_WORLD.getSize();
+        // Input parameter - an array size
+        int n = 4;
+        if (args.length != 0)
+        {
+            n = Integer.parseInt(args[0]);
+        }
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) a[i] = myrank;
+        int[] q = new int[n * np];
+        MPI.COMM_WORLD.gather(a, n, MPI.INT, q, n, MPI.INT, np - 1);
+        if (myrank == np - 1) {
+            for (int i = 0; i < q.length; i++)
+                System.out.println(" " + q[i]);
+        }
+        // Completion a parallel part
+        MPI.Finalize();
+    }
+}
+
+//        OUTPUT:
+//        0
+//        0
+//        0
+//        0
+//        1
+//        1
+//        1
+//        1
+//        2
+//        2
+//        2
+//        2
+//        3
+//        3
+//        3
+//        3
+
+
+
+
+
