dd393d6896001577ae23097161a59c76b6ee27f5
Denys Melnychenko <Denys Melnychenko@localhost>
2018-12-02 17:28:16 +0200

    my task

---
+++ src/main/java/com/mathpar/students/ukma/Melnychenko/MPI_2_16_TestReduceScatter.java
@@ -0,0 +1,71 @@
+package com.mathpar.students.ukma.Bohachek;
+
+import mpi.MPI;
+import mpi.MPIException;
+
+import java.util.Arrays;
+
+public class MPI_2_16_TestReduceScatter {
+
+    public static void main(String[] args) throws MPIException, InterruptedException {
+        MPI.Init(args);
+
+
+        int rank = MPI.COMM_WORLD.getRank();
+        int size = MPI.COMM_WORLD.getSize();
+
+        int[] arr = new int[size];
+        
+        for (int i = 0; i < size; ++i)
+        {
+            arr[i] = i;
+            System.out.println("rank = " + rank + "; arr[" + i + "] = " + arr[i]);
+        }
+
+        System.out.println();
+
+        int[] res = new int[1];
+
+        int[] recvSizes = new int[size];
+        Arrays.fill(recvSizes, 1);
+
+        MPI.COMM_WORLD.reduceScatter(arr, res, recvSizes, MPI.INT, MPI.SUM);
+
+        Thread.sleep(res.length * rank);
+
+        System.out.println("\nProc #" + rank + " received:");
+
+        for (int el : res)
+            System.out.println(el);
+
+        MPI.Finalize();
+    }
+}
+
+/*
+mpijavac -cp "/usr/local/lib/mpi.jar" MPI_2_16_TestReduceScatter.java
+mpirun java MPI_2_16_TestReduceScatter
+
+rank = 0; arr[0] = 0
+rank = 0; arr[1] = 1
+rank = 0; arr[2] = 2
+
+rank = 1; arr[0] = 0
+rank = 2; arr[0] = 0
+rank = 1; arr[1] = 1rank = 2; arr[1] = 1
+
+rank = 2; arr[2] = 2
+rank = 1; arr[2] = 2
+
+
+
+Proc #0 received:
+0
+
+Proc #1 received:
+3
+
+Proc #2 received:
+6
+
+*/
---
+++ src/main/java/com/mathpar/students/ukma/Melnychenko/MPI_3_2_Gather.java
@@ -0,0 +1,36 @@
+package com.mathpar.students.ukma.Bohachek;
+
+import java.util.Arrays;
+import mpi.*;
+
+public class MPI_3_2_Gather {
+    public static void main(String[] args)
+            throws Exception{
+        MPI.Init(args);
+        int myrank = MPI.COMM_WORLD.getRank();
+        int np = MPI.COMM_WORLD.getSize();
+        int n = Integer.parseInt(args[0]);
+        int[] a = new int[n];
+        for(int i = 0; i < n; i++) a[i] = myrank;
+        System.out.println("myrank = " + myrank + " : a = "
+                + Arrays.toString(a));
+        int[] q = new int[n*np];
+        MPI.COMM_WORLD.gather(a, n, MPI.INT, q, n, MPI.INT,
+                np-1);
+        if(myrank == np-1)
+            System.out.println("myrank = " + myrank + " : q = "
+                    + Arrays.toString(q));
+        MPI.Finalize();
+    }
+}
+
+
+/*
+mpijavac -cp "/usr/local/lib/mpi.jar" MPI_3_2_Gather.java
+mpirun java MPI_3_2_Gather 3
+
+myrank = 2 : a = [2, 2, 2]
+myrank = 0 : a = [0, 0, 0]
+myrank = 1 : a = [1, 1, 1]
+myrank = 2 : q = [0, 0, 0, 1, 1, 1, 2, 2, 2]
+ */
