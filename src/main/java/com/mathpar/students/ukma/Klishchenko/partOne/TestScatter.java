5ba0978c98848dc3273173ec757c01d167797021
ness-x540sa <nastia.nastia.zh@gmail.com>
2018-12-02 23:44:03 +0200

    Adding changes to Klishchenko package

---
+++ src/main/java/com/mathpar/students/ukma/Klishchenko/partOne/TestScatter.java
@@ -0,0 +1,36 @@
+package com.mathpar.students.ukma.Klishchenko.partOne;
+import mpi.*;
+
+public class TestScatter {
+    public static void main(String[] args) throws MPIException {
+        MPI.Init(args);
+        int myrank = MPI.COMM_WORLD.getRank();
+        int[] a = new int[2];
+        if (myrank == 0) {
+            for (int i = 0; i < 2; i++) {
+                a[i] = i;
+                System.out.println("a = " + a[i] + " ");
+            }
+            System.out.println("rank = " + myrank);
+        }
+        int[] q = new int[2];
+        MPI.COMM_WORLD.barrier();
+        MPI.COMM_WORLD.scatter(a, 1, MPI.INT, q, 1, MPI.INT, 0);
+        for (int i = 0; i < q.length; i++)
+            System.out.println("myrank = " + myrank + " ; " + q[i] + "\n");
+        MPI.Finalize();
+    }
+}
+
+// Output (2 proc) 
+//    a = 0
+//    a = 1
+//    rank = 0
+//    myrank = 0 ; 0
+//
+//    myrank = 0 ; 0
+//
+//    myrank = 1 ; 1
+//
+//    myrank = 1 ; 0
+
