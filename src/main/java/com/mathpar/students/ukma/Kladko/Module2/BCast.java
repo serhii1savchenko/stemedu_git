e6941fec5440829de8f726b86bfb8d98aadc41f1
Igor Grabar <igor47xgrabar@gmail.com>
2018-12-17 01:28:50 +0200

    Module 3

---
+++ src/main/java/com/mathpar/students/ukma/Kladko/Module2/BCast.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mathpar.students.ukma.Kladko.Module2;
+
+import java.util.Random;
+import mpi.Intracomm;
+import mpi.MPI;
+import mpi.MPIException;
+
+public class BCast 
+{
+    public static void main(String[] args) throws MPIException 
+    {
+        MPI.Init(args);
+        Intracomm WORLD = MPI.COMM_WORLD;
+
+        int rank = WORLD.getRank();
+        int arraySize = Integer.parseInt(args[0]);
+        double[] array = new double[arraySize];
+
+        if (rank == 0) 
+        {
+            //Generate array
+            System.out.print("Array = [ ");
+            for (int i = 0; i < arraySize; ++i) 
+            {
+                array[i] = new Random().nextDouble();
+                System.out.print(array[i]);
+                if (i != arraySize-1) System.out.print(", ");
+            }
+            System.out.println(" ]");
+        }
+
+        WORLD.barrier();
+
+        WORLD.bcast(array, arraySize, MPI.DOUBLE, 0);
+
+        if (rank != 0) 
+        {
+            //Generate array
+            System.out.print("Recieved array = [ ");
+            for (int i = 0; i < arraySize; ++i) 
+            {
+                System.out.print(array[i]);
+                if (i != arraySize-1) System.out.print(", ");
+            }
+            System.out.println(" ]");
+        }
+
+        MPI.Finalize();
+    }
+}
+/*
+Command:
+mpirun -np 4 java -cp /home/igorko/homework/stemedu/target/classes  com/mathpar/students/ukma/Grabar/Task2/BCast 5
+Output:
+Array = [ 0.8130071391305072, 0.9063344995024508, 0.6717712827908022, 0.6904512583167001, 0.17985381768401043 ]
+Recieved array = [ Recieved array = [ Recieved array = [ 0.8130071391305072, 0.9063344995024508, 0.6717712827908022, 0.6904512583167001, 0.17985381768401043 ]
+0.8130071391305072, 0.9063344995024508, 0.6717712827908022, 0.6904512583167001, 0.17985381768401043 ]
+0.8130071391305072, 0.9063344995024508, 0.6717712827908022, 0.6904512583167001, 0.17985381768401043 ]
+
+*/
---
+++ src/main/java/com/mathpar/students/ukma/Kladko/Module3/MPI4_2_SendObjects.java
@@ -0,0 +1,43 @@
+package com.mathpar.students.ukma.Kladko.Module3;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.nio.ByteBuffer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.mail.Transport;
+import mpi.MPI;
+import mpi.MPIException;
+
+import static com.mathpar.parallel.utils.MPITransport.sendObject;
+
+public class MPI4_2_SendObjects {
+    
+    public static void sendArrayOfObjects(Object[] a, int proc, int tag) 
+    throws MPIException, IOException {
+        for (int i = 0; i < a.length; i++)
+            sendObject(a[i], proc, tag + i);
+    }
+    
+    public static void sendObjects(Object[] a, int proc, int tag)
+    throws MPIException {
+        ByteArrayOutputStream bos = null;
+        try {bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos =
+            new ObjectOutputStream(bos);
+            for (Object a1 : a) {
+                oos.writeObject(a1);
+            }
+            bos.toByteArray();
+        } catch (IOException ex) {
+            Logger.getLogger(Transport.class.getName()).
+            log(Level.SEVERE, null, ex);
+        }
+        byte[] temp = bos.toByteArray();
+        ByteBuffer buf = MPI.newByteBuffer(temp.length);
+        buf.put(temp);
+        MPI.COMM_WORLD.iSend(buf, temp.length, MPI.BYTE, proc, tag);
+    }
+}
+
