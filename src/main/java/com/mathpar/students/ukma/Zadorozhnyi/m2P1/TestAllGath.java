be759d99b1ec4e3c554368d9355fe43392cf6a57
gennadi <gennadi@gennadi>
2018-12-18 00:55:33 +0200

    exam 18.12/18

---
+++ src/main/java/com/mathpar/students/ukma/Zadorozhnyi/m2P1/TestAllGath.java
@@ -0,0 +1,51 @@
+package com.mathpar.students.ukma.Zadorozhnyi.m2P1;
+
+import mpi.*;
+
+public class TestAllGath {
+    public static void main(String[] args) throws Exception {
+        MPI.Init(args);
+        Thread t = new Thread();
+        int myrank = MPI.COMM_WORLD.getRank();
+        int np = MPI.COMM_WORLD.getSize();
+        int n = 4;
+        if (args.length != 0)
+        {
+            n = Integer.parseInt(args[0]);
+        }
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = myrank;
+        }
+        int[] q = new int[n * np];
+        MPI.COMM_WORLD.allGather(a, n, MPI.INT, q, n, MPI.INT);
+        t.sleep(60 * myrank);
+        System.out.println("myrank = " + myrank + " : ");
+        for (int i = 0; i < q.length; i++) {
+            System.out.println(" " + q[i]);
+        }
+        System.out.println();
+        MPI.Finalize();
+    }
+}
+
+// Output for amount of processors equals 2 and n=4
+//        myrank = 0 :
+//        0
+//        0
+//        0
+//        0
+//        1
+//        1
+//        1
+//        1
+//
+//        myrank = 1 :
+//        0
+//        0
+//        0
+//        0
+//        1
+//        1
+//        1
+//        1
