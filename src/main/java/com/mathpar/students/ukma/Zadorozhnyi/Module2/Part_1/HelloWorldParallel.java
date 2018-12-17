6f6ffcb043035a532978135fcf8d98fa23579df9
Zadorozhnyi <Zadorozhnyi@localhost>
2018-12-17 10:18:14 +0200

    Fix packages

---
+++ src/main/java/com/mathpar/students/ukma/Zadorozhnyi/Module2/Part_1/HelloWorldParallel.java
@@ -0,0 +1,20 @@
+package com.mathpar.students.ukma.Zadorozhnyi.Module2.Part_1;
+import mpi.*;
+    public class HelloWorldParallel {
+        public static void main(String[] args)
+            throws MPIException {
+            //èíèöèàëèçàöèÿ ïàðàëëåëüíîé ÷àñòè
+            MPI.Init(args);
+            //îïðåäåëåíèå íîìåðà ïðîöåññîðà
+            int myrank = MPI.COMM_WORLD.getRank();
+            System.out.println("Proc num " + myrank + " Hello World");
+            //çàâåðøåíèå ïàðàëëåëüíîé ÷àñòè
+            MPI.Finalize();
+        }
+    }
+//mpirun java HelloWorldParallel
+// Output:
+//        Proc num 1 Hello World
+//        Proc num 2 Hello World
+//        Proc num 0 Hello World
+//        Proc num 3 Hello World
---
+++ src/main/java/com/mathpar/students/ukma/Zadorozhnyi/Module3/bcastObjectArray.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mathpar.students.ukma.Zadorozhnyi.Module3;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import mpi.*;
+
+public class bcastObjectArray {
+    public static void main(Object []o,int count, int root)throws IOException, MPIException,ClassNotFoundException
+    {
+        byte []tmp=null;
+        int []size=new int[1];
+        int rank=MPI.COMM_WORLD.getRank();
+        if (rank==root){
+            ByteArrayOutputStream bos = new ByteArrayOutputStream();
+            ObjectOutputStream oos = new ObjectOutputStream(bos);
+            for (int i=0; i<count; i++) oos.writeObject(o[i]);
+            tmp=bos.toByteArray();
+            size[0]=tmp.length;
+        }
+        MPI.COMM_WORLD.bcast(size,1,MPI.INT,root);
+        if (rank!=root) tmp=new byte[size[0]];
+        MPI.COMM_WORLD.bcast(tmp,tmp.length,MPI.BYTE,root);
+        if (rank!=root){
+            ByteArrayInputStream bis = new ByteArrayInputStream(tmp);
+            ObjectInputStream ois = new ObjectInputStream(bis);
+            for (int i=0; i<count; i++) o[i]=ois.readObject();
+        }
+    }
+}
+
+//mpirun java bcastObjectArray
