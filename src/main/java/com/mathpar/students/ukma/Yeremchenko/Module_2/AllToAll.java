a39c9f53127eacd47d10ad1f707823a7858132d5
Igor Grabar <igor47xgrabar@gmail.com>
2018-12-17 19:27:18 +0200

    Yeremchenko module 2 and 3

---
+++ src/main/java/com/mathpar/students/ukma/Yeremchenko/Module_2/AllToAll.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mathpar.students.ukma.Yeremchenko.Module_2;
+
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.Random;
+import mpi.Intracomm;
+import mpi.MPI;
+import mpi.MPIException;
+
+public class AllToAll 
+{
+    public static void main(String[] args) throws MPIException, InterruptedException 
+    {
+        MPI.Init(args);
+        Intracomm WORLD = MPI.COMM_WORLD;
+
+        int rank = WORLD.getRank();
+        int size = WORLD.getSize();
+        IntBuffer buffer = MPI.newIntBuffer(size);
+
+        for (int i = 0; i < buffer.capacity(); ++i)
+        {
+            buffer.put(i);
+            System.out.println("rank = " + rank + 
+                    "; buffer[" + i + "] = " + buffer.get(i) + ";");
+        }
+
+        System.out.println();
+
+        IntBuffer res = MPI.newIntBuffer(size);
+        WORLD.allToAll(buffer, 1, MPI.INT, res, 1, MPI.INT);
+        Thread.sleep(size * rank);
+
+        for (int i = 0; i < res.capacity(); i++)
+            System.out.println("rank = " + rank + 
+                    "; send = " + buffer.get(i) + 
+                    "; recv = " + res.get(i) + ";");
+
+        System.out.println();
+        MPI.Finalize();
+    }
+}
+/*
+Command:
+mpirun -np 4 java -cp /home/igorko/homework/stemedu/target/classes  com/mathpar/students/ukma/Yeremchenko/Module_2/AllToAll
+Outptu:
+rank = 0; buffer[0] = 0;
+rank = 0; buffer[1] = 1;
+rank = 0; buffer[2] = 2;rank = 1; buffer[0] = 0;
+rank = 0; buffer[3] = 3;
+
+rank = 1; buffer[1] = 1;
+
+rank = 1; buffer[2] = 2;
+rank = 1; buffer[3] = 3;
+
+rank = 3; buffer[0] = 0;
+rank = 3; buffer[1] = 1;
+rank = 3; buffer[2] = 2;
+rank = 3; buffer[3] = 3;
+
+rank = 2; buffer[0] = 0;
+rank = 2; buffer[1] = 1;
+rank = 2; buffer[2] = 2;
+rank = 2; buffer[3] = 3;
+
+rank = 0; send = 0; recv = 0;
+rank = 0; send = 1; recv = 0;
+rank = 0; send = 2; recv = 0;
+rank = 0; send = 3; recv = 0;
+
+rank = 1; send = 0; recv = 1;
+rank = 1; send = 1; recv = 1;
+rank = 1; send = 2; recv = 1;
+rank = 1; send = 3; recv = 1;
+
+rank = 2; send = 0; recv = 2;
+rank = 2; send = 1; recv = 2;
+rank = 2; send = 2; recv = 2;
+rank = 2; send = 3; recv = 2;
+
+rank = 3; send = 0; recv = 3;
+rank = 3; send = 1; recv = 3;
+rank = 3; send = 2; recv = 3;
+rank = 3; send = 3; recv = 3;
+
+
+*/
---
+++ src/main/java/com/mathpar/students/ukma/Yeremchenko/Module_2/Reduce.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mathpar.students.ukma.Yeremchenko.Module_2;
+
+import java.nio.IntBuffer;
+import java.util.Arrays;
+import java.util.Random;
+import mpi.Intracomm;
+import mpi.MPI;
+import mpi.MPIException;
+
+public class Reduce 
+{
+    public static void main(String[] args) throws MPIException, InterruptedException 
+    {
+        MPI.Init(args);
+        Intracomm WORLD = MPI.COMM_WORLD;
+
+        int rank = WORLD.getRank();
+        int size = WORLD.getSize();
+
+        int array[] = new int[size];
+
+        for (int i = 0; i < size; ++i)
+        {
+            array[i] = i;
+            System.out.println("rank = " + rank + "; arr[" + i + "] = " + array[i] + ";");
+        }
+
+        System.out.println();
+
+        int res[] = new int[size];
+
+        WORLD.reduce(array, res, size, MPI.INT, MPI.SUM, 0);
+
+        Thread.sleep(size * rank);
+        if (rank == 0) 
+        {
+            for (int i = 0; i < res.length; i++) System.out.print(res[i] + " ");
+            System.out.println();
+        }
+
+        MPI.Finalize();
+    }
+}
+/*
+Command:
+mpirun -np 4 java -cp /home/igorko/homework/stemedu/target/classes  com/mathpar/students/ukma/Yeremchenko/Module_2/Reduce
+Output:
+rank = 0; arr[0] = 0;
+rank = 0; arr[1] = 1;
+rank = 0; arr[2] = 2;
+rank = 0; arr[3] = 3;
+
+0 4 8 12 
+rank = 1; arr[0] = 0;
+rank = 1; arr[1] = 1;
+rank = 1; arr[2] = 2;
+rank = 1; arr[3] = 3;
+
+rank = 2; arr[0] = 0;
+rank = 2; arr[1] = 1;
+rank = 2; arr[2] = 2;
+rank = 2; arr[3] = 3;
+
+rank = 3; arr[0] = 0;
+rank = 3; arr[1] = 1;
+rank = 3; arr[2] = 2;
+rank = 3; arr[3] = 3;
+
+*/
