65bed3ee11a078a492ab0ed2a59f2f769475f3d5
Serhii Savchenko <savchenko.serhii@gmail.com>
2019-04-09 20:03:38 +0300

    block qr for 4x4

--- src/main/java/com/mathpar/students/savchenko/TestData.java
+++ src/main/java/com/mathpar/students/savchenko/TestData.java
@@ -8,12 +8,16 @@ import java.util.Random;
 public class TestData {
 
     public static MatrixD getTestMatrix(int n, Ring ring) {
+        return getTestMatrix(n, n, ring);
+    }
+
+    public static MatrixD getTestMatrix(int r, int c, Ring ring) {
         Random random = new Random(1);
 
-        int[][] matrix = new int[n][n];
+        int[][] matrix = new int[r][c];
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
                 matrix[i][j] = random.nextInt(10);
             }
         }
